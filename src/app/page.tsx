859fb94bbbcb61e4858cf06ca5e23595d4f8f0f3
winxene <winxeneric@gmail.com>
2025-05-16 23:40:54 +0700

    refactor: separated components from page level

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,9 @@
 "use client";
 
-import { ReactNode, useEffect, useState } from "react";
 import PageLayout from "./page-layout";
-
-type ReadmeCardProps = {
-  children: ReactNode;
-};
-
-const ReadmeCard = ({ children }: ReadmeCardProps) => {
-  return (
-    <div className="flex flex-col min-w-full mx-0 md:mx-24 2xl:mx-96 border border-suggestion rounded p-5">
-      <p className="text-subtitle text-xs md:text-sm py-4">README.md</p>
-      {children}
-    </div>
-  );
-};
-
-const SpotifyEmbed = () => {
-  //add loading mechanism later on
-  return (
-    <iframe
-      className="rounded-xl"
-      src="https://open.spotify.com/embed/track/6wKmxUeMJAoz2GpMrw95z5?utm_source=generator&theme=0"
-      width="40%"
-      height="152"
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      loading="lazy"
-    ></iframe>
-  );
-};
-
-const AsciiArt = ({ src }: { src: string }) => {
-  const [asciiArt, setAsciiArt] = useState("");
-
-  const fetchAsciiArt = async (textSrc: String) => {
-    const response = await fetch(textSrc.toString());
-    if (!response.ok) {
-      throw new Error("Failed to fetch ASCII art");
-    }
-    const data = await response.text();
-    return data;
-  };
-
-  useEffect(() => {
-    fetchAsciiArt(src)
-      .then((data) => setAsciiArt(data))
-      .catch((error) => console.error(error));
-  }, [src]);
-
-  return (
-    <pre className="text-[8px] text-center md:text-xs whitespace-pre-wrap">
-      {asciiArt}
-    </pre>
-  );
-};
+import AsciiArt from "@/components/assets/AsciiArt";
+import SpotifyEmbed from "@/components/assets/SpotifyEmbed";
+import ReadmeCard from "@/components/ui/ReadmeCard";
 
 const Home = () => {
   return (
@@ -63,7 +13,7 @@ const Home = () => {
     >
       <ReadmeCard>
         <div className="flex flex-col-reverse md:flex-row space-y-4 justify-center">
-          <div className="flex flex-col space-y-4 items-stretch">
+          <div className="flex flex-col space-y-4 text-left">
             <p>
               A passionate <span className="text-link">computer engineer </span>
               driven by tech innovation. Loves{" "}
@@ -72,15 +22,27 @@ const Home = () => {
               </span>
               . Currently learning mandarin and aiming to pass HSK 4.
             </p>
-            <p> My This Week's top jams:</p>
-            <SpotifyEmbed />
+            <div className="flex flex-col pt-0 md:pt-20 items-center space-y-0 md:space-y-10 md:items-start justify-center">
+              <p> My This Week's top jams:</p>
+              <SpotifyEmbed />
+            </div>
           </div>
           <AsciiArt src="/ascii/nemu-miyao.txt" />
         </div>
-        <p className="mt-20 mb-4">
+        <p className="mb-4">
           This whole website is based on my terminal look which you can access
-          from my dotfiles. You can use the “interactive” terminal to navigate.
-          Type help to see accepted commands.
+          from my{" "}
+          <a
+            href="https://github.com/winxene/ikura"
+            className="text-link hover:text-linkAlt"
+          >
+            dotfiles
+          </a>
+          . You can use the “interactive” terminal to navigate. Type{" "}
+          <a href="/help" className="text-link hover:text-linkAlt">
+            help
+          </a>{" "}
+          to see accepted commands.
         </p>
       </ReadmeCard>
     </PageLayout>
