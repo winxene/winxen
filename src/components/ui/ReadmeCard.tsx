2fc2650fdc9ff1a555b4f7b471bd06046756ba56
winxene <winxeneric@gmail.com>
2025-05-26 20:28:33 +0700

    fix: fixed the margin responsiveness

--- src/components/ui/ReadmeCard.tsx
+++ src/components/ui/ReadmeCard.tsx
@@ -6,7 +6,7 @@ type ReadmeCardProps = {
 
 const ReadmeCard = ({ children }: ReadmeCardProps) => {
   return (
-    <div className="flex flex-col w-[70%] mx-3 my-24 md:my-0 md:mx-auto border border-suggestion rounded p-5">
+    <div className="flex flex-col w-[70%] mx-3 my-24 md:mt-0 md:mx-auto border border-suggestion rounded p-5">
       <p className="text-subtitle text-xs md:text-sm py-4">README.md</p>
       {children}
     </div>
