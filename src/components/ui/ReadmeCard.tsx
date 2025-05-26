11ce844440893d84b2c0184be119959a9c658ce3
winxene <winxeneric@gmail.com>
2025-05-26 17:08:11 +0700

    fix: fixed the responsiveness by changing the default width and uses the mx-auto

--- src/components/ui/ReadmeCard.tsx
+++ src/components/ui/ReadmeCard.tsx
@@ -6,7 +6,7 @@ type ReadmeCardProps = {
 
 const ReadmeCard = ({ children }: ReadmeCardProps) => {
   return (
-    <div className="flex flex-col min-w-full mx-0 md:mx-24 2xl:mx-96 border border-suggestion rounded p-5">
+    <div className="flex flex-col w-[70%] mx-3 my-24 md:my-0 md:mx-auto border border-suggestion rounded p-5">
       <p className="text-subtitle text-xs md:text-sm py-4">README.md</p>
       {children}
     </div>
