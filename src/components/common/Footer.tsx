a1b2352c55b7eb6685015180d4c078867a55f53e
winxene <winxeneric@gmail.com>
2025-05-26 17:07:41 +0700

    chore: changed the width to w-3/5 for cleaner code

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 const Footer: FC = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="fixed bottom-0 min-w-full p-4 text-center text-xs md:text-sm  text-primary animate-fade-in-up">
+    <footer className="fixed bottom-0 w-3/5 p-4 text-center text-xs md:text-sm text-primary animate-fade-in-up">
       <a href="https://github.com/winxene/winxen">
         © {year} Winxen. All rights reserved.
       </a>
