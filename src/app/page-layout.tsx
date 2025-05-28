e8a853bf09c7105b09ec1557d9efb635e870864b
winxene <winxeneric@gmail.com>
2025-05-28 18:08:47 +0700

    chore: changed from using type to interface for better syntax consistency

--- src/app/page-layout.tsx
+++ src/app/page-layout.tsx
@@ -3,24 +3,28 @@ import Head from "next/head";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 
-type PageLayoutProps = {
+interface PageLayoutProps {
   title: string;
   contentDescription: string;
   children: ReactNode;
-};
+}
 
-const PageLayout = (props: PageLayoutProps) => {
+const PageLayout = ({
+  title,
+  contentDescription,
+  children,
+}: PageLayoutProps) => {
   return (
     <html lang="en">
       <body className="bg-bg text-primary">
         <Head>
-          <title>{props.title}</title>
-          <meta name="description" content={props.contentDescription} />
+          <title>{title}</title>
+          <meta name="description" content={contentDescription} />
           <meta name="robots" content="index, follow" />
         </Head>
         <main className="flex min-h-screen min-w-screen flex-col items-center justify-center px-0 md:px-24 md:pt-24 2xl:px-96 select-none">
           <Header />
-          {props.children}
+          {children}
           <Footer />
         </main>
       </body>
