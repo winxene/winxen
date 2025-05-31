0a2702bd72fb58463e5f98accf1a13951ac19a21
winxene <winxeneric@gmail.com>
2025-05-31 19:11:44 +0700

    feat: added constants for about-me pages

---
+++ src/constants/about-me/aboutMe.ts
@@ -0,0 +1,41 @@
+import {
+  faLinkedin,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+export const EXPERIENCE_DATA = [
+  "2024 - Now: Work as a freelancer and as a full-stack developer at Memilah, an AI waste bin solution.",
+  "2023: Part of Cohort 6 at Apple Developer Academy @BINUS.",
+  "2021 - 2022: Actively involved in BNCC, IEEE Student Branch @BINUS, Freshmen Leader, and Freshmen Partner for Engineering Faculty.",
+];
+
+export const ACHIEVEMENTS_DATA = [
+  "2024: Graduated Summa Cum Laude and honoured as Best Graduate of the Computer Engineering Program at BINUS 69th Graduation Ceremony.",
+  "2018: Achieved 3rd rank at National Chemistry Olympiad (City-level).",
+];
+
+export const CERTIFICATIONS_DATA = [
+  "2022: Alibaba Cloud Associate Cloud Computing",
+];
+
+export const SOCIAL_LINKS = [
+  {
+    href: "https://linkedin.com/in/winxene",
+    icon: faLinkedin,
+    color: "text-[#0077b5]",
+    label: "LinkedIn",
+  },
+  {
+    href: "https://instagram.com/winxenr",
+    icon: faInstagram,
+    color: "text-[#e4405f]",
+    label: "Instagram",
+  },
+  {
+    href: "https://github.com/winxene",
+    icon: faGithub,
+    color: "text-[#FFFFFF]",
+    label: "GitHub",
+  },
+];
