010aa46866179dde385b6b12926cadf037c316c2
winxene <winxeneric@gmail.com>
2025-06-06 15:53:01 +0700

    refactor: separated the regex logic into utils

--- src/components/common/FormattedText.tsx
+++ src/components/common/FormattedText.tsx
@@ -1,52 +1,49 @@
+import { markDownFormatter } from "@/utils/regex/markDownFormatter";
 import React, { FC } from "react";
 
-const FormattedText: FC<{ text: string }> = ({ text }) => {
-  const parseText = (text: string) => {
-    const parts = [];
-    let currentIndex = 0;
+interface Props {
+  text: string;
+}
 
-    const regex =
-      /(\b\d{4}(?:\s*-\s*(?:\d{4}|Now))?\b)|(\[([^\]]+)\]\(([^)]+)\))/g;
-    let match;
+const FormattedText: FC<Props> = ({ text }) => {
+  const segments = markDownFormatter(text);
 
-    while ((match = regex.exec(text)) !== null) {
-      if (match.index > currentIndex) {
-        parts.push(text.slice(currentIndex, match.index));
-      }
-
-      if (match[1]) {
-        parts.push(
-          <strong key={match.index} className="font-bold">
-            {match[1]}
-          </strong>,
-        );
-      } else if (match[2]) {
-        const linkText = match[3];
-        const linkUrl = match[4];
-        parts.push(
-          <a
-            key={match.index}
-            href={linkUrl}
-            className="text-link hover:text-linkAlt font-normal"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {linkText}
-          </a>,
-        );
-      }
-
-      currentIndex = regex.lastIndex;
-    }
-
-    if (currentIndex < text.length) {
-      parts.push(text.slice(currentIndex));
-    }
-
-    return parts;
-  };
-
-  return <span>{parseText(text)}</span>;
+  return (
+    <span>
+      {segments.map((seg, i) => {
+        switch (seg.type) {
+          case "text":
+            return <span key={i}>{seg.content}</span>;
+          case "bold":
+            return (
+              <strong key={i} className="font-bold">
+                {seg.content}
+              </strong>
+            );
+          case "link":
+            return (
+              <a
+                key={i}
+                href={seg.href}
+                className="text-link hover:text-linkAlt font-normal"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {seg.content}
+              </a>
+            );
+          case "year":
+            return (
+              <strong key={i} className="font-bold">
+                {seg.content}
+              </strong>
+            );
+          default:
+            return null;
+        }
+      })}
+    </span>
+  );
 };
 
 export default FormattedText;
