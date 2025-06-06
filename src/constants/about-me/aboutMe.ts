fee8629e2175f4b661e4cbe39fe3a1207e0a07eb
winxene <winxeneric@gmail.com>
2025-06-06 13:11:54 +0700

    refactor: added styling and colour list

--- src/constants/about-me/aboutMe.ts
+++ src/constants/about-me/aboutMe.ts
@@ -5,18 +5,38 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 export const EXPERIENCE_DATA = [
-  "2024 - Now: Work as a freelancer and as a full-stack developer at Memilah, an AI waste bin solution.",
-  "2023: Part of Cohort 6 at Apple Developer Academy @BINUS.",
-  "2021 - 2022: Actively involved in BNCC, IEEE Student Branch @BINUS, Freshmen Leader, and Freshmen Partner for Engineering Faculty.",
+  "2024 - Now: Work as a freelancer and as a full-stack developer at [Memilah](https://memilah.com), an AI waste bin solution.",
+  "2023: Part of Cohort 6 at [Apple Developer Academy @BINUS](https://drive.google.com/file/d/11c0XfrySNMgFQeyComd7UgX65gcnehIR/view?usp=drive_link).",
+  "2021 - 2022: Actively involved in [BNCC](https://bncc.net/), [IEEE Student Branch @BINUS](https://ieee.binus.ac.id/), Freshmen Leader, and Freshmen Partner for Engineering Faculty.",
 ];
 
 export const ACHIEVEMENTS_DATA = [
-  "2024: Graduated Summa Cum Laude and honoured as Best Graduate of the Computer Engineering Program at BINUS 69th Graduation Ceremony.",
+  "2024: Graduated [Summa Cum Laude](https://graduation.apps.binus.ac.id/graduates/2440066815/?ref) and honoured as [Best Graduate](https://graduation.apps.binus.ac.id/graduates/2440066815/?ref) of the Computer Engineering Program at BINUS 69th Graduation Ceremony.",
+  "2022: Finalist at [SEBELAS MARET International IoT Challenge 2022 competition](https://drive.google.com/file/d/1a7BU79PuJqtGA96l7xVvoDAfejWxUoW3/view).",
   "2018: Achieved 3rd rank at National Chemistry Olympiad (City-level).",
 ];
 
 export const CERTIFICATIONS_DATA = [
-  "2022: Alibaba Cloud Associate Cloud Computing",
+  "2022: [Alibaba Cloud Associate Cloud Computing](https://drive.google.com/file/d/1hCBIQboZBKmgwMOelnQlL3Nv-cH8rQvW/view)",
+];
+
+export const COLOURS = [
+  "bg-[#1f1f29]", // bg
+  "bg-[#d4323d]", // errorAlt
+  "bg-[#6e9565]", // subtitleAlt
+  "bg-[#c6a266]", // titleAlt
+  "bg-[#769ddd]", // linkAlt
+  "bg-[#958aac]", // domainAlt
+  "bg-[#5D9689]", // custom
+  "bg-[#cbc08d]", // primaryAlt
+  "bg-[#727168]", // suggestion
+  "bg-[#fd0007]", // error
+  "bg-[#8fbc61]", // subtitle
+  "bg-[#edc17a]", // title
+  "bg-[#6db6ce]", // link
+  "bg-[#987ebc]", // domain
+  "bg-[#6CAA9F]", // custom
+  "bg-[#ddd7b7]", // primary
 ];
 
 export const SOCIAL_LINKS = [
