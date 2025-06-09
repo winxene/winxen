5ed2445926fd792ad1c0820170dda81f36fcc18a
winxene <winxeneric@gmail.com>
2025-06-09 16:07:48 +0700

    chore: simplified categories name

--- src/constants/enums/ProjectCategory.ts
+++ src/constants/enums/ProjectCategory.ts
@@ -2,6 +2,6 @@ export enum ProjectCategory {
   PERSONAL = "Personal",
   INTERNSHIP = "Internship",
   APPRENTICESHIP = "Apprenticeship",
-  FREELANCE = "Freelance",
   WORK = "Work",
+  UNIVERSITY = "University",
 }
